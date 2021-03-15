18f292850b574410c28445512af64dc1b2496b21
Naman-Mtal <namanmittal153@gmail.com>
2021-03-15 11:16:09 +0530

    updated

--- employee-expense-system/src/main/java/com/cap/exs/controllers/LoginDetailsController.java
+++ employee-expense-system/src/main/java/com/cap/exs/controllers/LoginDetailsController.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
@@ -44,7 +45,16 @@ public class LoginDetailsController {
 	JwtUtils jwtUtils;
 	
 	
-	// login 
+	/**
+	 * This method is for logging in
+	 * 
+	 * @param LoginRequest
+	 * @return JwtResponse
+	 * @throws {@link UsernameNotFoundException}
+	 * @throws {@link MethodArgumentNotValidException}
+	 * @throws {@link BadCredentialsException}
+	 * 
+	 */
 	
 	@PostMapping("/signin")
 	@ApiOperation(value = "Signin", response = ResponseEntity.class)
